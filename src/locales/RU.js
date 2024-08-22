95b23f5bbe77631bd0b689e48a8a66bf3de99484
Spirin Sergey <spirin1504@mail.ru>
2024-08-22 14:14:03 +0300

    Изменил RU.js

--- src/locales/RU.js
+++ src/locales/RU.js
@@ -8,14 +8,35 @@ export default {
       about_us: "О нас",
       contact_us: "Связаться с нами",
     },
+    footer: {
+      working_ours: "Рабочее время",
+      monday: "Понедельник",
+      tuesday: "Вторник",
+      wednesday: "Среда",
+      thursday: "Четверг",
+      friday: "Пятница",
+      saturday: "Суббота",
+      sunday: "Воскресенье",
+      time_1: "9AM–5PM",
+      time_2: "Closed",
+      tel: "+1 305-804-6752",
+      adress: "1895 Tyler St, STE 404, Hollywood, FL 33020, US",
+      company: "© 2024 Polyus Consulting LLC",
+    },
     buttons: {
       apply_now: "Заказать услугу",
       submit: "Отправить",
+      accept: "Принять",
     },
     forms: {
       name: "Имя*",
       email: "Email*",
       message: "Сообщение",
+    },
+    cookie: {
+      title: "Уведомление. ",
+      description:
+        "TermsFeed использует файлы cookie для обеспечения необходимых функций веб-сайта, улучшения вашего опыта и анализа нашего трафика. Используя наш сайт, вы соглашаетесь с нашими",
     },
     block_1: {
       h1: "Подготовка годового налогового отчета",
@@ -25,53 +46,67 @@ export default {
       h2: "ВАЖНО!",
       p: "Существуют требования для всех американских компаний - подавать годовую налоговую отчетность в федеральное управление IRS и в штат, велась деятельность бизнеса или нет.",
       h3_1: "Календарь отчетности компаний в США",
-      green_table_1: {
-        h4: "Партнерство LLC (Два и более участников)",
-        p_1: "Форма 1065",
-        p_2: "Март",
-        p_3: "15",
-      },
-      green_table_2: {
-        h4: "Корпорация",
-        p_1: "Форма 1120",
-        p_2: "Апрель",
-        p_3: "15",
-      },
-      green_table_3: {
-        h4: "LLC - один частник",
-        p_1: "Форма 1040",
-        p_2: "Апрель",
-        p_3: "15",
-      },
+      green_table: [
+        {
+          h4: "Партнерство LLC (Два и более участников)",
+          p_1: "Форма 1065",
+          p_2: "Март",
+          p_3: "15",
+        },
+        {
+          h4: "Корпорация",
+          p_1: "Форма 1120",
+          p_2: "Апрель",
+          p_3: "15",
+        },
+        {
+          h4: "LLC - один частник",
+          p_1: "Форма 1040",
+          p_2: "Апрель",
+          p_3: "15",
+        },
+      ],
+
       h3_2: "Штрафы",
-      red_table_1: {
-        h4: "Просроченная сдача отчетности в IRS",
-        p_1: "Штраф:",
-        p_2: "$195/",
-        p_3: "за каждый месяц",
-      },
-      red_table_2: {
-        h4: "Просроченная сдача отчетности для Корпораций с участниками-нерезидентами",
-        p_1: "Штраф:",
-        p_2: "от",
-        p_3: "$25,000",
-      },
-      red_table_3: {
-        h4: "Неправильная подготовка информационных форм 5472 в IRS",
-        p_1: "Штраф:",
-        p_2: "от",
-        p_3: "$25,000",
-      },
+      red_table: [
+        {
+          h4: "Просроченная сдача отчетности в IRS",
+          p_1: "Штраф:",
+          t_2: "$195/",
+          t_3: "каждый месяц",
+        },
+        {
+          h4: "Просроченная сдача отчетности для Корпораций с участниками-нерезидентами",
+          p_1: "Штраф:",
+          p_2: "от",
+          p_3: "$25,000",
+        },
+        {
+          h4: "Неправильная подготовка информационных форм 5472 в IRS",
+          p_1: "Штраф:",
+          p_2: "от",
+          p_3: "$25,000",
+        },
+      ],
+
       p_b: "! В случае, если  Вы не успели подать/проверить данные для годовой отчетности, то IRS предоставляет отсрочку 6 месяцев без штрафов и пошлин. Для получения отсрочки, требуется подать корректную информацию в налоговую службу.",
     },
     block_3: {
       h2: "Преимущества обращения в Полюс Консалтинг:",
-      h3_1: "Профессионализм и опыт",
-      p_1: "Полюс Консалтинг - это команда квалифицированных специалистов с обширным опытом в налогообложении, гарантирующая соответствие ваших отчетов актуальным требованиями законодательства.",
-      h3_2: "Снижение рисков налоговых проверок",
-      p_2: "Правильная подготовка отчетности и минимизация ошибок снижают вероятность получения налоговых уведомлений и проверок. Специалисты компании тщательно проверяют все документы перед их подачей.",
-      h3_3: "Экономия времени",
-      p_3: "Подготовка налоговых отчетов требует значительного времени и усилий. Обратившись к профессионалам, вы освобождаете свои ресурсы для более важных бизнес-задач, что позволяет сосредоточиться на развитии и росте компании.",
+      list: [
+        {
+          h3: "Профессионализм и опыт",
+          p: "Полюс Консалтинг - это команда квалифицированных специалистов с обширным опытом в налогообложении, гарантирующая соответствие ваших отчетов актуальным требованиями законодательства.",
+        },
+        {
+          h3: "Снижение рисков налоговых проверок",
+          p: "Правильная подготовка отчетности и минимизация ошибок снижают вероятность получения налоговых уведомлений и проверок. Специалисты компании тщательно проверяют все документы перед их подачей.",
+        },
+        {
+          h3: "Экономия времени",
+          p: "Подготовка налоговых отчетов требует значительного времени и усилий. Обратившись к профессионалам, вы освобождаете свои ресурсы для более важных бизнес-задач, что позволяет сосредоточиться на развитии и росте компании.",
+        },
+      ],
     },
     block_4: {
       h2: "ПРОЦЕСС",
@@ -79,18 +114,26 @@ export default {
     },
     block_5: {
       h2: "ОТЗЫВЫ",
-      rewiew_1: {
-        name: "Айдар Мадаминов",
-        text: "Как владелец бизнеса во Флориде, я воспользовался услугами Полюс Консалтинг для подготовки налогового отчета и сэкономил массу времени и нервов. Все было подготовлено без ошибок и сдано в срок. Рекомендую!",
-      },
-      rewiew_2: {
-        name: "Светлана Романова",
-        text: "Искала помощь с годовым налоговым отчетом в США и обратилась в компанию Полюс Консалтинг. Лолита и ее команда профессионалы высокого уровня. Теперь являюсь их постоянным клиентом.",
-      },
-      rewiew_3: {
-        name: "Эдгар Лоуренс",
-        text: "Не в первый раз обращаюсь в Полюс Консалтинг и высоко оцениваю их ответственное отношение к работе. Они предоставляют отличное обслуживание и оптимальные решения для налогового отчета. Чувствую себя уверенно с ними!",
-      },
+      rewiew: [
+        {
+          name: "Айдар Мадаминов",
+          text: "Как владелец бизнеса во Флориде, я воспользовался услугами Полюс Консалтинг для подготовки налогового отчета и сэкономил массу времени и нервов. Все было подготовлено без ошибок и сдано в срок. Рекомендую!",
+          img: "/img/rewie_aidar.svg",
+          time: "08.14.2023",
+        },
+        {
+          name: "Светлана Романова",
+          text: "Искала помощь с годовым налоговым отчетом в США и обратилась в компанию Полюс Консалтинг. Лолита и ее команда профессионалы высокого уровня. Теперь являюсь их постоянным клиентом.",
+          img: "/img/rewie_svetlana.svg",
+          time: "03.27.2024",
+        },
+        {
+          name: "Эдгар Лоуренс",
+          text: "Не в первый раз обращаюсь в Полюс Консалтинг и высоко оцениваю их ответственное отношение к работе. Они предоставляют отличное обслуживание и оптимальные решения для налогового отчета. Чувствую себя уверенно с ними!",
+          img: "/img/rewie_aidar.svg",
+          time: "07.27.2024",
+        },
+      ],
     },
     block_6: {
       h2: "ЦЕНА",
@@ -98,7 +141,103 @@ export default {
       p: "Подготовка годовых налоговых отчетов с Полюс Консалтинг - для тех, кто ценит свое время!",
     },
     block_7: {
-      h2: "СВЯЗАТЬСЯ С НАМИ",
+      h2: "Связаться с нами",
+      p: "Нажимая кнопку «Отправить», вы даете согласие на обработку ваших персональных данных.",
     },
   },
+  PrivacyPolicy: [
+    {
+      h1: "Политика конфиденциальности",
+      p: "The Policy is designed to assist you in understanding how we collect, use and safeguard the personal information you provide to us and to assist you in making informed decisions when using our site and our products and services. This statement will be continuously assessed against new technologies, business practices and our customers’ needs.",
+    },
+    {
+      h2: "What Information Do We Collect?",
+      p: "When you visit our Web site you may provide us with two types of information: personal information you knowingly choose to disclose that is collected on an individual basis and Web site use information collected on an aggregate basis as you and others browse our Web site.",
+    },
+    {
+      h2: "1. Personal Information You Choose to Provide",
+      p_1: "For example, you may need to provide the following information:",
+      p_2: "In addition to providing the foregoing information, if you choose to correspond further with us through email, we may retain the content of your email messages together with your email address and our responses. We provide the same protections for these electronic communications that we employ in the maintenance of information received by mail and telephone.",
+      ul: [
+        "Name",
+        "Website URL information",
+        "Email address",
+        "Home and business phone number",
+      ],
+    },
+    {
+      h2: "2. Web Site Use Information",
+      p: "Similar to other commercial Web sites, our Web site utilizes a standard technology called “cookies” (see explanation below, “What Are Cookies?”) and Web server logs to collect information about how our Web site is used. Information gathered through cookies and Web server logs may include the date and time of visits, the pages viewed, time spent at our Web site, and the Web sites visited just before and just after our Web site. This information is collected on an aggregate basis. None of this information is associated with you as an individual.",
+    },
+    {
+      h2: "How Do We Use the Information That You Provide to Us?",
+      p: "Broadly speaking, we use personal information for purposes of administering our business activities, providing customer service and making available other products and services to our customers and prospective customers. Occasionally, we may also use the information we collect to notify you about new services and special offers we think you will find valuable. The lists used to send you product and service offers are developed and managed and designed to safeguard the security and privacy of our customers’ personal information. As a customer, you will be given the opportunity. What Are Cookies? Cookies are a feature of Web browser software that allows Web servers to recognize the computer used to access a Web site. Cookies are small pieces of data that are stored by a user’s Web browser on the user’s hard drive. Cookies can remember what information a user accesses on one Web page to simplify subsequent interactions with that Web site by the same user or to use the information to streamline the user’s transactions on related Web pages. This makes it easier for a user to move from Web page to Web page and to complete commercial transactions over the Internet. Cookies should make your online experience easier and more personalized.",
+    },
+    {
+      h2: "How Do We Use Information We Collect from Cookies?",
+      p: "We use Web site browser software tools such as cookies and Web server logs to gather information about our Web site users’ browsing activities, in order to constantly improve our Web site and better serve our customers. This information assists us to design and arrange our Web pages in the most user-friendly manner and to continually improve our Web site to better meet the needs of our customers and prospective customers. Cookies help us collect important business and technical statistics. The information in the cookies lets us trace the paths followed by users to our Web site as they move from one page to another. Web server logs allow us to count how many people visit our Web site and evaluate our Web site’s visitor capacity. We do not use these technologies to capture your individual email address or any personally identifying information about you although they do permit us to send focused online banner advertisements or other such responses to you.",
+    },
+    {
+      h2: "How Do We Protect Your Information?",
+      p: "We utilize encryption/security best practices to safeguard the confidentiality of personal information we collect from unauthorized access or disclosure and accidental loss, alteration or destruction.",
+    },
+    {
+      h2: "Communication Opt Out",
+      p: "If you wish to opt out of receiving offers directly from PolyusConsulting, you can unsubscribe by following the opt-out instructions in the emails that they send you located at the footer of the communication. We can be reached by postal mail at 1895 Tyler St, STE 404, Hollywood, FL 33020, or by phone at +1 305-804-6752. You can email us at info@polyusconsulting.com with questions, comments or suggestions.",
+    },
+    {
+      h2: "Do We Disclose Information to Outside Parties?",
+      p: "We may provide aggregate information about our customers, sales, Web site traffic patterns and related Web site information to our affiliates or reputable third parties, but this information will not include personally identifying data, except as otherwise provided in this Privacy Policy.",
+    },
+    {
+      h2: "What about Legally Compelled Disclosure of Information?",
+      p: "We may disclose information when legally compelled to do so, in other words, when we, in good faith, believe that the law requires it or for the protection of our legal rights.",
+    },
+    {
+      h2: "What About Other Web Sites Linked to Our Web Site?",
+      p: "We are not responsible for the practices employed by Web sites linked to or from our Web site nor the information or content contained therein.",
+    },
+  ],
+  TermsOfUs: [
+    {
+      h1: "Условия эксплуатации",
+      p: "These terms and conditions govern your use of this web site. By accessing this web site, you are acknowledging and accepting these terms of use. These terms of use are subject to change by Polyus Consulting at any time and at our discretion without notice. Your use of this web site after any changes are implemented constitutes your acceptance of the changes. As a result, we encourage you to consult the terms and conditions each time you use this web site.",
+    },
+    {
+      h2: "1. INTELLECTUAL PROPERTY",
+      p: "You acknowledge and agree that all content and materials available on this site are protected by copyrights, trademarks, service marks, patents, trade secrets, or other proprietary rights and laws. Except as expressly authorized by Polyus Consulting, you agree not to sell, license, rent, modify, distribute, copy, reproduce, transmit, publicly display, publicly perform, publish, adapt, edit, or create derivative works from such materials or content. As noted above, reproduction, copying, or redistribution for commercial purposes of any materials or design elements on this site is strictly prohibited without the express written permission of Polyus Consulting. For information on requesting such permission, please contact us at info@polyusconsulting.com.",
+    },
+    {
+      h2: "2. THIRD PARTY REFERENCES / HYPERLINKS",
+      p: "This site may link you to other sites on the Internet. These sites may contain information or material that some people may find inappropriate or offensive. These other sites are not under the control of Polyus Consulting, and you acknowledge that Polyus Consulting is not responsible for the accuracy, copyright compliance, legality, decency, or any other aspect of the content of such sites. The inclusion of such a link does not imply endorsement of the site by or any association with its operators.",
+    },
+    {
+      h2: "3. CONTACTING US",
+      p: "If you need to contact us, you can email us at info@polyusconsulting.com, call us at +1 305-804-6752, or send us a letter at: 1895 Tyler St, STE 404, Hollywood, FL 33020.",
+    },
+    {
+      h2: "4. DISCLAIMER OF WARRANTIES",
+      p: "ALL MATERIALS, INFORMATION, SOFTWARE, PRODUCTS, AND SERVICES INCLUDED IN OR AVAILABLE THROUGH THIS SITE (THE “CONTENT”) ARE PROVIDED “AS IS” AND “AS AVAILABLE” FOR YOUR USE. THE CONTENT IS PROVIDED WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR NONINFRINGEMENT. POLYUS CONSULTING AND ITS AGENTS DO NOT WARRANT THAT THE CONTENT IS ACCURATE, RELIABLE OR CORRECT; THAT THIS SITE WILL BE AVAILABLE AT ANY PARTICULAR TIME OR LOCATION; THAT ANY DEFECTS OR ERRORS WILL BE CORRECTED; OR THAT THE CONTENT IS FREE OF VIRUSES OR OTHER HARMFUL COMPONENTS. YOUR USE OF THIS SITE IS SOLELY AT YOUR RISK. BECAUSE SOME JURISDICTIONS DO NOT PERMIT THE EXCLUSION OF CERTAIN WARRANTIES, THESE EXCLUSIONS MAY NOT APPLY TO YOU.",
+    },
+    {
+      h2: "5. LIMITATION OF LIABILITY",
+      p: "UNDER NO CIRCUMSTANCES SHALL POLYUS CONSULTING OR ITS AGENTS BE LIABLE FOR ANY DIRECT, INDIRECT, PUNITIVE, INCIDENTAL, SPECIAL, OR CONSEQUENTIAL DAMAGES THAT RESULT FROM THE USE OF, OR INABILITY TO USE, THIS SITE. THIS LIMITATION APPLIES WHETHER THE ALLEGED LIABILITY IS BASED ON CONTRACT, TORT, NEGLIGENCE, STRICT LIABILITY, OR ANY OTHER BASIS, EVEN IF $ClientName HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. BECAUSE SOME JURISDICTIONS DO NOT ALLOW THE EXCLUSION OR LIMITATION OF INCIDENTAL OR CONSEQUENTIAL DAMAGES, LIABILITY FOR POLYUS CONSULTING IN SUCH JURISDICTIONS SHALL BE LIMITED TO THE EXTENT PERMITTED BY LAW.",
+    },
+    {
+      h2: "6. INDEMNIFICATION",
+      p: "Upon a request by Polyus Consulting, you agree to defend, indemnify, and hold Polyus Consulting and its other affiliated companies harmless, and their employees, contractors, officers, and directors from all liabilities, claims, and expenses, including attorney’s fees, that arise from your misuse of this site.",
+    },
+    {
+      h2: "7. SEVERABILITY AND INTEGRATION",
+      p: "Unless otherwise specified herein, this agreement constitutes the entire agreement between you and Polyus Consulting with respect to this site and supersedes all prior or contemporaneous communications between you and Polyus Consulting with respect to this site. If any part of these Terms of Use is held invalid or unenforceable, that portion shall be construed in a manner consistent with applicable law to reflect, as nearly as possible, the original intentions of the parties, and the remaining portions shall remain in full force and effect.",
+    },
+    {
+      h2: "8. CALL TO ACTION DISCLOSURE",
+      p: "Direct contact SMS will be used to contact you related to website and website lead capture form for Polyus Consulting and the dental services advertised herein. Message and data rates may apply. Check with your carrier for more information. Direct contact SMS will be used to contact you related to website and website lead capture form for Polyus Consulting and the dental services advertised herein. Message and data rates may apply. Check with your carrier for more information. You may contact customer support by typing “HELP” Read our https://polyusconsulting.com/en/privacy-policy/ for more information about how your data is stored and shared.",
+    },
+    {
+      h2: "9. JURISDICTION",
+      p: "These Terms of Use shall be governed by and construed in accordance with the laws of the State where Polyus Consulting is domiciled. You hereby consent to binding arbitration in the State of FL domicile to resolve any disputes arising under this Terms of Use.",
+    },
+  ],
 };
